1d38bf181e7471a5829f2f790553d5d6abf70bac
teeaz <teeaz@github.com>
2021-12-12 15:54:55 +0800

    minor

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,11 +1,13 @@
 import Layout from "../comp/layout"
 import { findOne, find } from "../lib/find"
 import MarkdownView from "../comp/markdown"
-import { Heading } from "@theme-ui/components"
+import { Box, Heading } from "@theme-ui/components"
 
 const PostPage = ({ data, content }) => (
   <Layout title={data.title}>
     <Heading as="h1">{data.title}</Heading>
+    <Box  color="textLite"><i>{data.date.substr(0,10)}</i></Box>
+
     <MarkdownView>{content}</MarkdownView>
   </Layout>
 )
