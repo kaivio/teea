1f0451925eb496e70a48c141933da2d01ef490c3
teeaz <teeaz@github.com>
2021-12-19 13:11:37 +0800

    编写管理脚本

---
+++ tool/page.init.js
@@ -0,0 +1,17 @@
+// pages/$path
+import Layout from "../comp/layout"
+import {A} from '../comp/base'
+import * as lib from '../lib'
+
+
+export default function $comp(props){
+  return (<Layout title="$title">
+    
+  </Layout>)
+}
+
+export async function getStaticProps(){
+  let props = { }
+
+  return { props }
+}
